d5f3cb84d0a97664965cbe801d95d0b4b1da9762
weimeng.mao <weimeng.mao>
2025-03-17 16:42:55 +0800

    初始化

---
+++ apps/web-ele/src/api/core/user.ts
@@ -0,0 +1,21 @@
+/**
+ * 获取用户信息
+ */
+export async function getUserInfoApi() {
+  // return requestClient.get<UserInfo>('/user/info');
+  return {
+    /**
+     * 用户描述
+     */
+    desc: 'mwmwmw',
+    /**
+     * 首页地址
+     */
+    homePath: '',
+
+    /**
+     * accessToken
+     */
+    token: 'wwwwwwwww',
+  };
+}
